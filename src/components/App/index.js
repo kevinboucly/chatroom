eeac8a609f9d22e858c163624bf52cb7a9b18122
kevin <kevinboucly9@gmail.com>
2020-08-20 13:07:34 +0200

    pb de socket

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,5 @@
 // == Import npm
-import React from 'react';
+import React, { useEffect } from 'react';
 
 // == Import
 import Form from 'src/containers/Form';
@@ -8,15 +8,23 @@ import Settings from 'src/containers/Settings';
 
 import './style.scss';
 
-
 // == Composant
-const App = () => (
-  <div className="app">
-    <Messages />
-    <Form />
-    <Settings />
-  </div>
-);
-
+const App = () => {
+  useEffect(() => {
+    console.log('je veux me connecter au websocket');
+    const socket = window.io('http://localhost:3001');
+    socket.on('send_message', (info) => {
+      console.log('un message a été envoyé', info);
+    });
+    socket.emit('send_message', { content:'salut', author:'keke'});
+  }, []);
+  return (
+    <div className="app">
+      <Messages />
+      <Form />
+      <Settings />
+    </div>
+  );
+};
 // == Export
 export default App;
